a161e507aff29aa2d2adcfad052dd5e29fad9dcc
VolcanoCookies <franek.kg@gmail.com>
2021-11-12 12:22:19 +0100

    Fixed negel and orel

--- Rules.pl
+++ Rules.pl
@@ -19,22 +19,16 @@ impint(I1, I2, _, Proofs, Line, imp(C1, C2)) :-
     Line is I2+1,
     last([[I1, C1, _]|T], [I2, C2, _]).
 
-negel(I1, I2, _, Proofs, Proof) :-
-    trace,
-    conclusion(Proofs, Proof, I2, neg(X)),
-    conclusion(Proofs, Proof, I1, X),
+negel(I1, I2, _, Proofs, [Line, cont, Name]) :-
+    conclusion(Proofs, [Line, cont, Name], I2, neg(X)),
+    conclusion(Proofs, [Line, cont, Name], I1, X),
     I1>I2.
 
-orel(I1, I2, I3, I4, I5, _, Proofs, _, Conclusion) :-
-    I2 is I1+1,
-    I4 is I3+1,
-    I1<I2,
-    I2<I3,
-    I3<I4,
-    proof(Proofs, I1, or(X, Y)),
-
-    conclusion(I1, or(X, Y)),
-    deep_contains(Proofs, [[I2, X, assumption]|Next1]),
-    last(Next1, [I3, cont, _]),
-    deep_contains(Proofs, [[I4, Y, assumption]|Next2]),
-    last(Next2, [I5, cont, _]).
+orel(I1, I2, I3, I4, I5, _, Proofs, [Line, Conclusion, Name]) :-
+    box(Proofs, [Line, Conclusion, Name], I2, I3, Box1),
+    first_proof(Box1, [_, C1, _]),
+    box(Proofs, [Line, Conclusion, Name], I4, I5, Box2),
+    first_proof(Box2, [_, C2, _]),
+    proof(Proofs, [Line, Conclusion, Name], I1, [_, or(C1, C2), _]),
+    last_proof(Box1, [_, Conclusion, _]),
+    last_proof(Box2, [_, Conclusion, _]).
